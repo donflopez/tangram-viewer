562a3965c33cf256e6c8a4ea2c3308fdb2811e77
Francisco López <donflopez@gmail.com>
2016-07-07 09:22:12 +0200

    Add landing example page

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -1,2 +1,16 @@
 import { FlowRouter } from 'meteor/kadira:flow-router';
 import { BlazeLayout } from 'meteor/kadira:blaze-layout';
+
+// Load layouts
+// const layouts = '/imports/ui/layouts/';
+
+import '/imports/ui/layouts/landing.js';
+
+function render() {
+  BlazeLayout.render(this.name);
+}
+
+FlowRouter.route('/landing', {
+  name: 'landing',
+  action: render,
+});
